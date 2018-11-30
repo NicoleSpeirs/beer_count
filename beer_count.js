1a527d797a6c93ed37b99a236ee7eb4086973948
Nicole Speirs <nickispeirs@gmail.com>
2018-11-30 14:09:34 -0600

    add beer count song

---
+++ beer_count.js
@@ -0,0 +1,14 @@
+var beerCount = 99
+
+
+    while(beerCount >= 1){
+        var bottleNum = "bottles"
+        if (beerCount == 1){ bottles = "bottle"}
+
+        console.log(beerCount + " " + bottleNum + " of beer on the wall,");
+        console.log(beerCount + " " + "of beer,");
+        console.log("Take one down, pass it arround,");
+      beerCount--;
+        console.log(beerCount + " " + bottleNum + " of beer on the wall.");
+    }
+
